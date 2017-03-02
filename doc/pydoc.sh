7cdfec23f6b2e85854ac6e812d0c33f563cd2521
Ryan Berthold <r.berthold@eaobservatory.org>
2017-03-01 15:42:30 -1000

    add pydoc script; ignore generated doc files

---
+++ doc/pydoc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd `dirname "$0"`
+/local/python/bin/pydoc -w drama
+/local/python/bin/pydoc -w drama.__drama__
+/local/python/bin/pydoc -w drama.log
+/local/python/bin/pydoc -w drama.qt4
+/local/python/bin/pydoc -w drama.rts
+/local/python/bin/pydoc -w drama.version
